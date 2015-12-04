334f7d9734f3b77fe2cf5b4b929642d0832d532a
Emmanuelle Raffenne <eraffenne@keedio.com>
2015-12-04 13:13:26 +0100

    IMASD-50: Overload request methods

--- src/main/java/org/keedio/examples/rest/Request.java
+++ src/main/java/org/keedio/examples/rest/Request.java
@@ -3,16 +3,14 @@ package org.keedio.examples.rest;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 
 public class Request {
@@ -21,6 +19,13 @@ public class Request {
 
     private URI query;
     private RestTemplate restTemplate;
+
+    public Request(String endpoint) throws URISyntaxException {
+
+        this.query = new URI(endpoint);
+        this.restTemplate = new RestTemplate();
+        log.info("URI is: " + query.toString());
+    }
 
     public Request(String endpoint, Map<String, ?> params) {
 
@@ -32,7 +37,6 @@ public class Request {
 
         this.query = builder.build().encode().toUri();
         this.restTemplate = new RestTemplate();
-
         log.info("URI is: " + query.toString());
     }
 
@@ -41,17 +45,21 @@ public class Request {
             throws IOException {
 
         if (!headers.containsKey(HttpHeaders.CONTENT_TYPE)) {
-            headers.set(HttpHeaders.CONTENT_TYPE, "application/octet-stream");
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
         }
         HttpEntity<byte[]> entity = new HttpEntity<>(IOUtils.toByteArray(new FileInputStream(file)), headers);
 
         return restTemplate.exchange(query, HttpMethod.POST, entity, clazz);
     }
 
+    public ResponseEntity<?> postText(String body, Class<?> clazz) {
+        return postText(body, new HttpHeaders(), clazz);
+    }
+
     public ResponseEntity<?> postText(String body, HttpHeaders headers, Class<?> clazz) {
 
         if (!headers.containsKey(HttpHeaders.CONTENT_TYPE)) {
-            headers.set(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded");
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         }
         HttpEntity<String> entity = new HttpEntity<>(body, headers);
 
@@ -61,4 +69,10 @@ public class Request {
     public Object get(Class<?> clazz) {
         return restTemplate.getForObject(query, clazz);
     }
+
+    public ResponseEntity<?> get(Class<?> clazz, HttpHeaders headers) {
+        HttpEntity<String> entity = new HttpEntity<>("", headers);
+        return restTemplate.exchange(query, HttpMethod.GET, entity, clazz);
+    }
+
 }
