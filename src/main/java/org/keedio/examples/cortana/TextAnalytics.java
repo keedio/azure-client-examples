0fda7ca62c89fcf958b35a747ad15daf0c7ff5ce
Emmanuelle Raffenne <eraffenne@keedio.com>
2015-12-04 14:54:38 +0100

    IMASD-50: Implements text analytics (sentiment, key phrases, language detection)

--- src/main/java/org/keedio/examples/cortana/TextAnalytics.java
+++ src/main/java/org/keedio/examples/cortana/TextAnalytics.java
@@ -2,6 +2,7 @@ package org.keedio.examples.cortana;
 
 import org.keedio.examples.IService;
 import org.keedio.examples.rest.Request;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -15,15 +16,28 @@ import java.util.List;
 import java.util.Map;
 
 @Component
-@Profile("sentiment")
-public class Sentiment extends CortanaService implements IService {
+@Profile("textanalytics")
+public class TextAnalytics extends CortanaService implements IService {
 
-    public Sentiment() {
+    @Value("${service}")
+    private String service;
+
+    private static final Map<String, String> svc2method;
+    static {
+        svc2method = new HashMap<String, String>();
+        svc2method.put("sentiment", "GetSentiment");
+        svc2method.put("keyphrases", "GetKeyPhrases");
+        svc2method.put("language", "GetLanguage");
+    }
+
+    public TextAnalytics() {
         super();
     }
 
     @Override
     public Object request(String text) throws IOException {
+
+        String url = endpoint + "/" + svc2method.get(service);
 
         List<MediaType> mediaTypes = new ArrayList<>();
         mediaTypes.add(MediaType.APPLICATION_JSON);
@@ -35,7 +49,7 @@ public class Sentiment extends CortanaService implements IService {
         Map<String, String> params = new HashMap<>();
         params.put("Text", text);
 
-        Request request = new Request(endpoint, params);
+        Request request = new Request(url, params);
         ResponseEntity<?> sentiment = request.get(String.class, headers);
 
         return sentiment.getBody();
